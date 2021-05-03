ef9d9c8b3935a78c1dda06c5e7be3c70f1264b6a
ishan dudeja <ishandudeja@gmail.com>
2021-05-04 07:16:24 +1200

    Initial commit

---
+++ ViewModel/StudentViewModel.cs
@@ -0,0 +1,57 @@
+﻿using MVVMDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MVVMDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public StudentViewModel()
+        {
+            ButtonCommand = new RelayCommand(new Action<object>(ShowMessage));
+            LoadStudents();
+        }
+
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = students;
+        }
+
+
+        private ICommand m_ButtonCommand;
+        public ICommand ButtonCommand
+        {
+            get
+            {
+                return m_ButtonCommand;
+            }
+            set
+            {
+                m_ButtonCommand = value;
+            }
+        }
+
+        public void ShowMessage(object obj)
+        {
+            MessageBox.Show(obj.ToString());
+        }
+    }
+}
